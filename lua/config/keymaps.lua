fdef797d8d6864a66d23401ad88b691bbcdc5c3a
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-08-09 00:30:47 +0200

    Add some custom key-mappings

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,20 @@
+local keymap = vim.api.nvim_set_keymap
+local default_opts = { noremap = true, silent = true }
+local expr_opts = { noremap = true, expr = true, silent = true }
+
+-- Better escape using jk in insert and terminal mode
+keymap("i", "kj", "<Esc>", default_opts)
+keymap("t", "kj", "<C-\\><C-n>", default_opts)
+
+-- Center search results
+keymap("n", "n", "nzz", default_opts)
+keymap("n", "N", "Nzz", default_opts)
+
+-- Move selected line / block of text in visual mode
+keymap("x", "K", ":move '<-2<CR>gv-gv", default_opts)
+keymap("x", "J", ":move '>+1<CR>gv-gv", default_opts)
+
+keymap("n", ",,", "<C-^>", default_opts)
+
+keymap("n", "msg", ":messages <CR>", default_opts)
+keymap("n", "msgg", ":messages clear<CR>", default_opts)
