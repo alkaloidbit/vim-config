f823fac1d47f88b1eca5351a7ecab56fc60b1876
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-08-09 00:42:14 +0200

    Configure Nord Theme options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,11 @@ local opt = vim.opt
 opt.foldmethod = "indent"
 
 opt.colorcolumn=""
+
+vim.g.nord_contrast = false
+vim.g.nord_borders = false
+vim.g.disable_background = false
+vim.g.nord_cursorline_transparent = false
+vim.g.nord_italic = false
+
+
