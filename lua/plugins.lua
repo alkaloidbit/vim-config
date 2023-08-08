8aea0bb72dbdc67f4bbe90f41a4f9f20056ad134
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-08-08 23:46:42 +0200

    Add initial customisation

---
+++ lua/plugins.lua
@@ -0,0 +1,17 @@
+return {
+	{ import = 'rafi.plugins.extras.coding.copilot' },
+	{ 'rafi/awesome-vim-colorschemes', enabled = false },
+	{ 'alkaloidbit/nord.nvim' },
+	{
+		'rafi/tabstrip.nvim',
+		opts = {
+			colors = {
+				modified = { fg = '#434C5E', ctermfg = 2 },
+			},
+			icons = {
+				modified = '',
+				session = '',
+			},
+		},
+	},
+}
