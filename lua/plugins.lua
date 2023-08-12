2f2b61d06c48f39dc8926f26fcdc78c184cd594a
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-08-12 23:22:55 +0200

    Statusline: Using feline

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,38 +1,37 @@
 return {
 	-- { import = 'rafi.plugins.extras.coding.copilot' },
-	{ 'rafi/awesome-vim-colorschemes', enabled = false },
-	{ 'nvim-lualine/lualine.nvim', enabled = false },
-	{ 'alkaloidbit/nord.nvim', branch = "localchanges" },
+	{ "rafi/awesome-vim-colorschemes", enabled = false },
+	{ "nvim-lualine/lualine.nvim", enabled = false },
+	{ "alkaloidbit/nord.nvim", branch = "localchanges" },
 	{
-		'rafi/tabstrip.nvim',
+		"rafi/tabstrip.nvim",
 		opts = {
 			colors = {
-				modified = { fg = '#eceff4', ctermfg = 2 },
-
+				modified = { fg = "#eceff4", ctermfg = 2 },
 			},
 			icons = {
 
-				modified = '●',
-				session = '',
+				modified = "●",
+				session = "",
 			},
 		},
 		branch = "localchanges",
 	},
 	{
-		'feline-nvim/feline.nvim', enabled = false
+		"feline-nvim/feline.nvim",
 	},
 	{
-		'nvimdev/whiskyline.nvim',
-		enabled = true,
-		dependencies = 'nvim-tree/nvim-web-devicons',
+		"nvimdev/whiskyline.nvim",
+		enabled = false,
+		dependencies = "nvim-tree/nvim-web-devicons",
 	},
 	{
-		'rebelot/heirline.nvim',
+		"rebelot/heirline.nvim",
 		enabled = false,
 		module = false,
 	},
-	{ import = 'rafi.plugins.extras.coding.emmet' },
-	{ import = 'rafi.plugins.extras.editor.ufo' },
-	{ import = 'rafi.plugins.extras.editor.flybuf' },
-	{ import = 'rafi.plugins.extras.lsp.lightbulb' },
+	{ import = "rafi.plugins.extras.coding.emmet" },
+	{ import = "rafi.plugins.extras.editor.ufo" },
+	{ import = "rafi.plugins.extras.editor.flybuf" },
+	{ import = "rafi.plugins.extras.lsp.lightbulb" },
 }
