2e026feb47d51f66baffac4f1360386286ca3f91
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-09-15 15:26:29 +0200

    Install Codeium co-pilot

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -16,6 +16,18 @@ return {
 		branch = "localchanges",
 	},
 	{
+		"Exafunction/codeium.vim",
+		event = "InsertEnter",
+		config = function ()
+			vim.g.codeium_disable_bindings = 1
+      vim.keymap.set("i", "<A-m>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<A-f>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<A-b>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<A-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("i", "<A-s>", function() return vim.fn["codeium#Complete"]() end, { expr = true })
+		end
+	},
+	{
 		"RRethy/vim-illuminate",
 		opts = {
 			providers = {
