67698abf65d4cbe0431292da68a11a7c09e473f9
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-09-19 00:13:58 +0200

    Override cmp-nvim with codeium source

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,137 @@ return {
 		opts = { lsp = { progress = { enabled = false }}}
 	},
 	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			{ "jcdickinson/codeium.nvim", config = true },
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-emoji",
+			{ "saadparwaiz1/cmp_luasnip", dependencies = "L3MON4D3/LuaSnip" },
+			"andersevenrud/cmp-tmux",
+			"hrsh7th/cmp-cmdline",
+		},
+		opts = function()
+			local cmp = require("cmp")
+			local defaults = require("cmp.config.default")()
+			local luasnip = require("luasnip")
+
+			local function has_words_before()
+				if vim.bo.buftype == "prompt" then
+					return false
+				end
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				-- stylua: ignore
+				return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+			end
+
+			return {
+				preselect = cmp.PreselectMode.None,
+				sorting = defaults.sorting,
+				experimental = {
+					ghost_text = {
+						hl_group = "Comment",
+					},
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp", priority = 50 },
+					{ name = "codeium", priority = 50 },
+					{ name = "path", priority = 40 },
+					{ name = "luasnip", priority = 30 },
+				}, {
+					{ name = "buffer", priority = 50, keyword_length = 3 },
+					{ name = "emoji", insert = true, priority = 20 },
+					{
+						name = "tmux",
+						priority = 10,
+						keyword_length = 3,
+						option = { all_panes = true, label = "tmux" },
+					},
+				}),
+				mapping = cmp.mapping.preset.insert({
+					-- <CR> accepts currently selected item.
+					-- Set `select` to `false` to only confirm explicitly selected items.
+					["<CR>"] = cmp.mapping({
+						i = function(fallback)
+							if cmp.visible() and cmp.get_active_entry() then
+								cmp.confirm({ select = false })
+							else
+								fallback()
+							end
+						end,
+						s = cmp.mapping.confirm({
+							select = true,
+							behavior = cmp.ConfirmBehavior.Replace,
+						}),
+						-- Do not set command mode, it will interfere with noice popmenu.
+					}),
+					["<S-CR>"] = cmp.mapping.confirm({
+						behavior = cmp.ConfirmBehavior.Replace,
+						select = true,
+					}),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-d>"] = cmp.mapping.select_next_item({ count = 5 }),
+					["<C-u>"] = cmp.mapping.select_prev_item({ count = 5 }),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-c>"] = function(fallback)
+						cmp.close()
+						fallback()
+					end,
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.jumpable(1) then
+							luasnip.jump(1)
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
+				formatting = {
+					format = function(entry, vim_item)
+						-- Prepend with a fancy icon from config lua/rafi/config/init.lua
+						local icons = require("rafi.config").icons
+						if entry.source.name == "git" then
+							vim_item.kind = icons.git
+						elseif entry.source.name == "codeium" then
+							vim_item.kind = "^"
+						else
+							local symbol = icons.kinds[vim_item.kind]
+							if symbol ~= nil then
+								vim_item.kind = symbol .. " " .. vim_item.kind .. " " .. entry.source.name
+
+							end
+						end
+						return vim_item
+					end,
+				},
+			}
+		end,
+	},
+
+	{
 		"j-hui/fidget.nvim",
 		tag = "legacy",
 		event = "LspAttach",
@@ -56,6 +187,7 @@ return {
 	},
 	{
 		"Exafunction/codeium.vim",
+		enabled = false,
 		event = "BufEnter",
 		config = function()
 			vim.g.codeium_disable_bindings = 1
