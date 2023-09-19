003f6da93c0e30e1d3e770c71504d1e9bb068829
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-09-20 00:20:02 +0200

    nvim-ufo config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,9 +1,7 @@
 return {
 	{ "junegunn/fzf", build = "./install --bin" },
 	{ "folke/tokyonight.nvim", opts = { style = "storm" } },
-	{ 'folke/noice.nvim',
-		opts = { lsp = { progress = { enabled = false }}}
-	},
+	{ "folke/noice.nvim", opts = { lsp = { progress = { enabled = false } } } },
 	{
 		"hrsh7th/nvim-cmp",
 		event = "InsertEnter",
@@ -125,7 +123,6 @@ return {
 							local symbol = icons.kinds[vim_item.kind]
 							if symbol ~= nil then
 								vim_item.kind = symbol .. " " .. vim_item.kind .. " " .. entry.source.name
-
 							end
 						end
 						return vim_item
@@ -138,15 +135,15 @@ return {
 		"codota/tabnine-nvim",
 		build = "./dl_binaries.sh",
 		event = "VeryLazy",
-		config = function ()
-			require("tabnine").setup {
+		config = function()
+			require("tabnine").setup({
 				disable_auto_comment = true,
 				accept_keymap = "<A-t>",
 				dismiss_keymap = "<C-]>",
 				debounce_ms = 800,
 				suggestion_color = { gui = "#808080", cterm = 244 },
 				exclude_filetypes = { "TelescopePrompt" },
-			}
+			})
 		end,
 	},
 	{
@@ -164,6 +161,82 @@ return {
 		config = function()
 			-- calling `setup` is optional for customization
 			require("fzf-lua").setup({})
+		end,
+	},
+
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			{
+				"luukvbaal/statuscol.nvim",
+				config = function()
+					local builtin = require("statuscol.builtin")
+					require("statuscol").setup({
+						relculright = true,
+						segments = {
+							{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
+							{ text = { "%s" }, click = "v:lua.ScSa" },
+							{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+						},
+					})
+				end,
+			},
+		},
+		-- stylua: ignore
+		keys = {
+			{ "zc" },
+      { "zo" },
+      { "zC" },
+      { "zO" },
+      { "za" },
+      { "zA" },
+			{ 'zr', function() require('ufo').openFoldsExceptKinds() end, desc = "Open Folds Except Kinds", },
+			{ 'zR', function() require('ufo').openAllFolds() end, desc = "Open All Folds" },
+			{ 'zM', function() require('ufo').closeAllFolds() end, desc = "Close All Folds" },
+			{ 'zm', function() require('ufo').closeFoldsWith() end, desc = "Close Folds With"},
+			{ "zp", function()
+					local winid = require('ufo').peekFoldedLinesUnderCursor()
+					if not winid then
+						vim.lsp.buf.hover()
+					end
+				end, desc = "Peek Fold", },
+		},
+		opts = {
+			fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local totalLines = vim.api.nvim_buf_line_count(0)
+				local foldedLines = endLnum - lnum
+				local suffix = ("  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				local rAlignAppndx = math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+				suffix = (" "):rep(rAlignAppndx) .. suffix
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
+			end,
+		},
+		config = function(_, opts)
+			require("ufo").setup(opts)
 		end,
 	},
 	{ "rafi/awesome-vim-colorschemes", enabled = false },
@@ -324,7 +397,7 @@ return {
 	{ import = "rafi.plugins.extras.diagnostics.proselint" },
 	{ import = "rafi.plugins.extras.diagnostics.write-good" },
 	{ import = "rafi.plugins.extras.editor.anyjump" },
-	{ import = "rafi.plugins.extras.editor.ufo" },
+	-- { import = "rafi.plugins.extras.editor.ufo" },
 	{ import = "rafi.plugins.extras.editor.flybuf" },
 	{ import = "rafi.plugins.extras.formatting.prettier" },
 	{ import = "rafi.plugins.extras.lsp.gtd" },
