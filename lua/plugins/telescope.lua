91a7caae5b3d11e25dd4a2031332ea2757389594
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-10-01 04:05:46 +0200

    Style: customizing Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,14 +3,24 @@ return {
 		"nvim-telescope/telescope.nvim",
 		dependencies = {
 			"stevearc/aerial.nvim",
+			 "nvim-treesitter/nvim-treesitter", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
 		},
 		keys = {
 			{
 				"<leader>co", "<cmd>Telescope aerial<cr>", desc = "Code Outline"
 			}
 		},
+		opts = function ()
+			return require "plugins.configs.telescope"
+		end,
 		config = function(_, opts)
-			require("telescope").load_extension("aerial")
+			local telescope = require "telescope"
+			telescope.setup(opts)
+
+			-- load extensions
+			for _, ext in ipairs(opts.extensions_list) do
+				telescope.load_extension(ext)
+			end
 		end
 	},
 
