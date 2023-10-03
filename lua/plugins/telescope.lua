5936793f23305cb2d7ed48a5eb5ac315e8ccb422
fredbadlieutenant <fredoudjoudi@gmail.com>
2023-10-03 23:48:58 +0200

    Telescope: customizing highlights (nord theme)

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -26,9 +26,23 @@ return {
 			-- Highlights
 			local fg_bg = require("utils").fg_bg
 			local colors = require("plugins.colorscheme.nord.named_colors")
+			fg_bg("TelescopePromptPrefix", colors.red, colors.black)
+			fg_bg("TelescopeNormal", colors.red, colors.black)
 			fg_bg("TelescopePreviewTitle", colors.black, colors.green)
 			fg_bg("TelescopePromptTitle", colors.black, colors.red)
 			fg_bg("TelescopeResultsTitle", colors.black, colors.blue)
+			fg_bg("TelescopeSelection", colors.white, colors.black)
+			fg_bg("TelescopeResultsDiffAdd", colors.green, colors.black)
+			fg_bg("TelescopeResultsDiffChange", colors.yellow, colors.black)
+			fg_bg("TelescopeResultsDiffDelete", colors.red, colors.black)
+
+
+			fg_bg("TelescopeBorder", colors.black, colors.black)
+			fg_bg("TelescopePromptBorder", colors.black, colors.black)
+			fg_bg("TelescopePromptNormal", colors.white, colors.black)
+			fg_bg("TelescopeResultsTitle", colors.black, colors.black)
+			fg_bg("TelescopePromptPrefix", colors.red, colors.black)
+
 		end,
 	},
 
